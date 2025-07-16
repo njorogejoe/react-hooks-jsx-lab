fbb36a2f20a0cf1d72fdd7c6d6e22f7feee15868
Joseph <mwanikij118@gmail.com>
2025-07-16 10:26:37 +0300

    Adds Home.js, About.js, Navbar.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <img src={image} alt=" I made this" />
+      <p></p>
+    </div>
+  );
 }
 
 export default About;
